9de192f7ad128cf54265e00bd8adf9cae0c70652
diipacheco <edilsonpacheco6@gmail.com>
2021-03-04 20:58:22 -0300

    Adiciona registro e listagem de apontamentos no banco de dados

--- src/database/migrations/1614899741793-CreateAppointments.ts
+++ src/database/migrations/1614899741793-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1614899741793
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
