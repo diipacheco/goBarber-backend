688861af7da3d2bd4d501b55086a84e42e7d45c2
diipacheco <edilsonpacheco6@gmail.com>
2021-03-05 19:29:11 -0300

    Adiciona upload de imagens e atualizaçao do avatar do usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -19,5 +26,32 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const { id } = request.user;
+
+      const { filename } = request.file;
+
+      const updateAvatar = new UpdateUserAvatarService();
+
+      const user = await updateAvatar.execute({
+        user_id: id,
+        avatarFileName: filename,
+      });
+
+      // @ts-expect-error
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
