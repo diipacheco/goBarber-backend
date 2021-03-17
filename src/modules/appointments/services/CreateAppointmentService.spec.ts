e4baf5cef17e2ca8bbfab75e318be40ac40402a9
diipacheco <edilsonpacheco6@gmail.com>
2021-03-17 05:50:34 -0300

    Finaliza testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,5 +19,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123123213');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {});
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2021, 3, 17, 5);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123123123213',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123123123213',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
