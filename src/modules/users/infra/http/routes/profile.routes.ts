8f9fd521226f690012944f20da4444fd904e3d60
diipacheco <edilsonpacheco6@gmail.com>
2021-03-23 04:33:39 -0300

    adiciona validaçao nas rotas de put e put

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ProfileController from '../controllers/ProfileController';
 
@@ -10,7 +11,25 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/', profileController.update);
-profileRouter.get('/', profileController.show);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {},
+  }),
+  profileController.show,
+);
 
 export default profileRouter;
