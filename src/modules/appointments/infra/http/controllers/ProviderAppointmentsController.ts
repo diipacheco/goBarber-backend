219a47249b251e466a43f8bc483f08924ed18ad6
diipacheco <edilsonpacheco6@gmail.com>
2021-05-07 21:02:23 -0300

    Serilialize the appointments cache

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListAppointmentsService from '@modules/appointments/services/ListAppointmentsService';
 
@@ -17,7 +18,7 @@ class ProviderAppointmentsController {
       year,
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
 
